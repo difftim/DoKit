5b71f617e408415c2d6122592e914251c2aa5da7
Jaymin <jiangcanming1992@gmail.com>
2025-11-25 19:34:18 +0800

    disable uploading data

--- iOS/DoraemonKit/Src/Core/Util/BuriedPoint/DoraemonBuriedPointManager.m
+++ iOS/DoraemonKit/Src/Core/Util/BuriedPoint/DoraemonBuriedPointManager.m
@@ -85,19 +85,19 @@
 }
 
 - (void)uploadData{
-    if(self.pointArray.count > 0){
-        [self.basicInfoDic setValue:self.pointArray forKey:@"events"];
-        NSMutableDictionary *params = [self.basicInfoDic copy];
-
-        [DoraemonNetworkUtil postWithUrlString:@"https://www.dokit.cn/pointData/addPointData" params:params success:^(NSDictionary * _Nonnull result) {
-            NSInteger code = [result[@"code"] integerValue];
-            if (code == 200) {
-                [self removePointArray];
-            }
-        } error:^(NSError * _Nonnull error){
-            
-        }];
-    }
+//    if(self.pointArray.count > 0){
+//        [self.basicInfoDic setValue:self.pointArray forKey:@"events"];
+//        NSMutableDictionary *params = [self.basicInfoDic copy];
+//
+//        [DoraemonNetworkUtil postWithUrlString:@"https://www.dokit.cn/pointData/addPointData" params:params success:^(NSDictionary * _Nonnull result) {
+//            NSInteger code = [result[@"code"] integerValue];
+//            if (code == 200) {
+//                [self removePointArray];
+//            }
+//        } error:^(NSError * _Nonnull error){
+//            
+//        }];
+//    }
 }
 
 
